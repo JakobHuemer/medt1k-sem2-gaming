08751967874fd91078df1d233d0a3e96391ec26e
Jakob Huemer-Fistelberger <j.huemer-fistelberger@students.htl-leonding.ac.at>
2023-05-09 21:31:01 +0200

    changed webport temporary

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import path from 'path';
 const app = express();
 const port = 3000;
 const webApp = express();
-const webPort = 80;
+const webPort = 8080;
 
 // create http server from webApp
 const server = http.createServer(webApp);
